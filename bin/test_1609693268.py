46e40b8a71a919ede250ee5b11c87770a9e2e5b2
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-01-03 18:01:15 +0100

    Created bin/test_1609693268.py

---
+++ bin/test_1609693268.py
@@ -0,0 +1,17 @@
+"""Test 1609693268 file
+"""
+from 1609693268 import *
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-1-3"
+
+def test():
+	"""Tests the 1609693268 function in the 1609693268 class
+	Write here all test you want to do.
+	REMEMBER to test your programm you can't use __init__ function
+	"""
+	assert 1609693268.1609693268() == "1609693268", "test failed"
+	#assert 1609693268.<function>(<values>) == <the result(s) you would like to have>, "<the fail message>"
+	
+if __name__ == "__main__":
+	test()
