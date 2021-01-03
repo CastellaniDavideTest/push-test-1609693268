8da6c4b21ff8fea90b52e1536a7b812662549fe2
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-01-03 18:01:13 +0100

    Created bin/1609693268.py

---
+++ bin/1609693268.py
@@ -0,0 +1,19 @@
+"""1609693268
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-1-3"
+
+class 1609693268:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(1609693268.1609693268())
+	
+	def 1609693268():
+		"""1609693268 first funtion
+		"""
+		return "1609693268" #for the test (see test_1609693268.py)
+		
+if __name__ == "__main__":
+	1609693268()
